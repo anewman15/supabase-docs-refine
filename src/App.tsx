f41bfe94890f59a8a46270f51d2bfdc299adb945
Abdullah Numan <anewman15@hotmail.com>
2023-06-01 01:38:15 +0600

    feat(docs): add routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Authenticated, GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import routerBindings, {
+  CatchAllNavigate,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { dataProvider, liveProvider } from "@refinedev/supabase";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
 import authProvider from "./authProvider";
 import { supabaseClient } from "./utility";
+import Account from "./components/account";
+import Auth from "./components/auth";
 
 function App() {
   return (
@@ -26,7 +29,26 @@ function App() {
           }}
         >
           <Routes>
-            <Route index element={<WelcomePage />} />
+          <Route
+            element={
+              <Authenticated
+                  fallback={<CatchAllNavigate to="/login" />}
+              >
+                  <Outlet />
+              </Authenticated>
+            }
+          >
+            <Route index element={<Account />} />
+          </Route>
+          <Route
+            element={
+              <Authenticated
+                fallback={<Outlet />}
+              />
+            }
+          >
+            <Route path="/login" element={<Auth />} />
+          </Route>
           </Routes>
           <RefineKbar />
           <UnsavedChangesNotifier />
